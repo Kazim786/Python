c248d91749c7724f64f27571769c564db2705915
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-01-17 09:52:06 -0600

    Some notes too

--- Hwk_Thursday.py
+++ Hwk_Thursday.py
@@ -16,14 +16,14 @@
 
 #4
 
-number_entry #= int(input("Enter a number "))
+# number_entry #= int(input("Enter a number "))
 
-def is_even(any_number):
-    if (any_number % 2 == 0 ):
-        print("True")
-    else:
-        print("false")
-print(is_even(number_entry))
+# def is_even(any_number):
+#     if (any_number % 2 == 0 ):
+#         print("True")
+#     else:
+#         print("false")
+# print(is_even(number_entry))
 
 #5
 # odd_number = int(input("Write an odd number "))
@@ -52,7 +52,7 @@ print(is_even(number_entry))
 
 # GET HELP ON THIS ^^^^^^^^
 
-
+#5 & 6 for SMALL SECTION GET HELP
 
 
 # Mediums:
@@ -64,6 +64,21 @@ print(is_even(number_entry))
 
 
 # print(smallest_Number(my_Choice))
+
+#2 (Nicholas's method):
+# def large(x):
+#     y = x[0]
+#     for i in x:
+#         if i > y:
+#             y = i
+ 
+#     return(y)
+# print(large( [2,3,4,55, 1,6]))
+
+
+
+#2
+
 # bigList = [3, 9, 8, 10]
 # def largest_Number(big_boy_list):
 #     return max(big_boy_list)
@@ -72,6 +87,21 @@ print(is_even(number_entry))
 # short_stringList = ["Little" , "Red", "Ridinghood"]
 
 # def shortString(the_Short_String):
+
+
+
+# 3 - Find the shortest String
+# ============================
+# list_o_strings = ['hello', 'goodbye', 'Kearney', 'let\'s go!', 'Ohio State', 'more', 'lesson', 'help!']
+# def shortest(string_list):
+#     shortest_str = string_list[0]
+#     for each_string in string_list:
+#         if len(each_string) < len(shortest_str):
+#             shortest_str = each_string            
+#     return shortest_str
+# print(f'Your list of strings is: {list_o_strings}')
+# print(f'The shortest string in your list is: {shortest(list_o_strings)}')
+
 
 
 
