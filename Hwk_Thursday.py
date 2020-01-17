b9b39ad6b1d6a59b00c929af66f47049d118375c
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-01-16 22:59:26 -0600

    will need help tomorrow

--- Hwk_Thursday.py
+++ Hwk_Thursday.py
@@ -16,20 +16,47 @@
 
 #4
 
-# number_entry = int(input("Enter a number "))
+number_entry #= int(input("Enter a number "))
 
-# def is_even(any_number):
-#     if (any_number % 2 == 0 ):
-#         print("True")
-#     else:
-#         print("false")
-# print(is_even(number_entry))
+def is_even(any_number):
+    if (any_number % 2 == 0 ):
+        print("True")
+    else:
+        print("false")
+print(is_even(number_entry))
+
+#5
+# odd_number = int(input("Write an odd number "))
+
+# def is_odd(any_number):
+#     is_even(any_number)
+#     return print("true")  
+
+# print(is_odd(odd_number))
+# GET HELP ON THIS ONE
+
+#6
+# thheList = [1, 2, 4, 8, 10, 6]
+# the_Evens = []
+# def only_evens(even_steven):
+    
+#     for i in even_steven:
+#         if (is_even(even_steven)):
+#             the_Evens.append(i)
+#         else:
+#             print("Keep trying")
+
+#         return print(the_Evens)
+
+# print(only_evens(thheList))
+
+# GET HELP ON THIS ^^^^^^^^
 
 
 
 
 # Mediums:
-
+#1
 # my_Choice = [3, 9, 8, 10]
 
 # def smallest_Number(my_List):
@@ -42,7 +69,10 @@
 #     return max(big_boy_list)
 # print(largest_Number(bigList))
 
-short_stringList = ["Little, Red, Ridinghood"]
-def shortString(the_Short_String):
-    return min(the_Short_String)
+# short_stringList = ["Little" , "Red", "Ridinghood"]
+
+# def shortString(the_Short_String):
+
+
+
 
