53e1bceeb90d00b86a54a08a1680eca2cd7cf4c9
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-01-16 20:20:03 -0600

    Thursday week 1 hwk

--- Hwk_Thursday.py
+++ Hwk_Thursday.py
@@ -1,7 +1,11 @@
-my_Choice = [3, 9, 8, 10]
+# my_Choice = [3, 9, 8, 10]
 
-def smallest_Number(my_List):
-    return min(my_List)
+# def smallest_Number(my_List):
+#     return min(my_List)
 
 
-print(smallest_Number(my_Choice))
+# print(smallest_Number(my_Choice))
+bigList = [3, 9, 8, 10]
+def largest_Number(big_boy_list):
+    return max(big_boy_list)
+print(largest_Number(bigList))
