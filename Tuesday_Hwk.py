c4c6e346ba7a59b58d8003177ddbbdff1b5b221f
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-01-20 14:06:26 -0600

    Fixed stuff

--- Tuesday_Hwk.py
+++ Tuesday_Hwk.py
@@ -1,23 +1,25 @@
 # 1 & 2
 total_bill = float(input("What is your total? "))
 service = input("What was the level of service? Good, fair, or bad ")
-how_many_people = float(input("How many people are there? "))
 if (service == "Good"):
     tip = total_bill * .20
     total_amount = tip + total_bill
     print(total_amount)
+    how_many_people = float(input("How many people are there? "))
     the_split = total_amount/how_many_people
     print(the_split)
 elif (service == "fair"):
     tip = total_bill * .15
     total_amount = tip + total_bill
     print(total_amount)
+    how_many_people = float(input("How many people are there? "))
     the_split = total_amount/how_many_people
     print(the_split)
 elif (service == "Bad"):
     tip = total_bill * .10
     total_amount = tip + total_bill
     print(total_amount)
+    how_many_people = float(input("How many people are there? "))
     the_split = total_amount/how_many_people
     print(the_split)
 else:
