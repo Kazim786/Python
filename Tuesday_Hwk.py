4d4221b94292e76c2a2f2003bdc22a185079d81e
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-01-20 14:37:22 -0600

    more work done

--- Tuesday_Hwk.py
+++ Tuesday_Hwk.py
@@ -1,26 +1,36 @@
 # 1 & 2
-total_bill = float(input("What is your total? "))
-service = input("What was the level of service? Good, fair, or bad ")
-if (service == "Good"):
-    tip = total_bill * .20
-    total_amount = tip + total_bill
-    print(total_amount)
-    how_many_people = float(input("How many people are there? "))
-    the_split = total_amount/how_many_people
-    print(the_split)
-elif (service == "fair"):
-    tip = total_bill * .15
-    total_amount = tip + total_bill
-    print(total_amount)
-    how_many_people = float(input("How many people are there? "))
-    the_split = total_amount/how_many_people
-    print(the_split)
-elif (service == "Bad"):
-    tip = total_bill * .10
-    total_amount = tip + total_bill
-    print(total_amount)
-    how_many_people = float(input("How many people are there? "))
-    the_split = total_amount/how_many_people
-    print(the_split)
-else:
-    print("INVALID ENTRY ")
+# total_bill = float(input("What is your total? "))
+# service = input("What was the level of service? Good, fair, or bad ")
+# if (service == "Good"):
+#     tip = total_bill * .20
+#     total_amount = tip + total_bill
+#     print(total_amount)
+#     how_many_people = float(input("How many people are there? "))
+#     the_split = total_amount/how_many_people
+#     print(the_split)
+# elif (service == "fair"):
+#     tip = total_bill * .15
+#     total_amount = tip + total_bill
+#     print(total_amount)
+#     how_many_people = float(input("How many people are there? "))
+#     the_split = total_amount/how_many_people
+#     print(the_split)
+# elif (service == "Bad"):
+#     tip = total_bill * .10
+#     total_amount = tip + total_bill
+#     print(total_amount)
+#     how_many_people = float(input("How many people are there? "))
+#     the_split = total_amount/how_many_people
+#     print(the_split)
+# else:
+#     print("INVALID ENTRY ")
+
+
+# 6
+#Multiplication table
+# for outter in range(1, 11):
+#     for inner in range(1, 11):
+#         multiply = outter * inner
+#         # print(outter, "X", inner, "=", multiply)
+#         # 2 Different ways to print this
+#         #print(f'{outter} X {inner} = {multiply}')
